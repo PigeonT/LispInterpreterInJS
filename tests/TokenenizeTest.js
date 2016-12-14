bfdff244fc7230699e7ddf316be632fc1d73f6a8
pigeonT <pigeon.tian@gmail.com>
2016-12-14 22:02:28 +0100

    updates

--- tests/TokenenizeTest.js
+++ tests/TokenenizeTest.js
@@ -1,3 +1,14 @@
-const assert = require('assert');
+const assert = require('chai').assert,
+      expect = require('chai').expect,
+      should = require('chai').should;
 
-describe('')
+describe('test InputStream', () => {
+    let InputStream = require('../InputStream.js');
+     
+     
+});
+
+describe('test Tokenenize', () => {
+
+
+});
